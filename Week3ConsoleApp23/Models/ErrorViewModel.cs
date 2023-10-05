2213d1f3fe811a1a0a778bfd464854e70677ac48
Jack Monaghan <s00227213@atu.ie>
2023-10-05 10:37:55 +0100

    S00227213 Initial Project structure complete

---
+++ Week3ConsoleApp23/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Week3ConsoleApp23.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
