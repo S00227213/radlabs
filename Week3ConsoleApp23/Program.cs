0bc3ef8a6adb0b3e6ca344040ce719f55af08083
Jack Monaghan <s00227213@atu.ie>
2023-10-05 11:26:35 +0100

    Two-tier Console lab finished

--- Week3ConsoleApp23/Program.cs
+++ Week3ConsoleApp23/Program.cs
@@ -1,3 +1,4 @@
+using Tracker.WebAPIClient;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,12 +6,18 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+        ActivityAPIClient.Track(
+    StudentID: "S00227213",
+    StudentName: "Jack Monaghan",
+    activityName: "Rad301 23 Week 3 Lab 1",
+    Task: "Starting two tier Lab"
+);
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts();  // The default HSTS value is 30 days. You may want to change this for production scenarios.
 }
 
 app.UseHttpsRedirection();
