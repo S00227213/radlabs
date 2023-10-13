f2f1182e9ddd9f92ebfeea577c89241fba74135f
Jack Monaghan <s00227213@atu.ie>
2023-10-13 13:02:50 +0100

    Added Week 4 Sample App

---
+++ Week3MauiLabS00227213/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+using Plugin.Maui.Audio;
+using Week3MauiLabS00227213.ViewModels;
+using System;
+
+namespace Week3MauiLabS00227213
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        MainViewModel viewModel;
+
+        public MainPage(IAudioManager audioManager)
+        {
+            InitializeComponent();
+            viewModel = new MainViewModel(audioManager);
+            this.BindingContext = viewModel;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterBtn.Text = count == 1 ? $"Clicked {count} time" : $"Clicked {count} times";
+            viewModel.PlaySound("Resources/Raw/sound_29.wav");
+        }
+    }
+}
---
+++ Week3MauiLabS00227213/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Plugin.Maui.Audio;
+
+namespace Week3MauiLabS00227213
+{
+    public static class MauiProgram
+    {
+        public static MauiApp AppInstance;
+
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            // Add the IAudioManager service
+            builder.Services.AddSingleton(AudioManager.Current);
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            AppInstance = builder.Build();
+            return AppInstance;
+        }
+    }
+}
