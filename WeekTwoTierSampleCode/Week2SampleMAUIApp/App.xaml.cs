7a8c1d922876b975ef2097c1bfbd8022cf9fec96
Jack Monaghan <s00227213@atu.ie>
2023-10-26 23:25:31 +0100

    Two-tier Console lab finished

---
+++ WeekTwoTierSampleCode/Week2SampleMAUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Week2SampleMAUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WeekTwoTierSampleCode/Week2SampleMAUIApp/Model Views/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Plugin.Maui.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2SampleMAUIApp.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public IAudioManager AudioManager { get; set; }
+        public IAudioPlayer AudioPlayer { get; set; }
+        public MainViewModel(IAudioManager audioManager) {
+            AudioManager = audioManager;
+        }
+
+        [ObservableProperty]
+        List<string> soundNames = new List<string>() { "0.wav", "1.wav"};
+
+        [RelayCommand]
+        public void PlaySound(string Sound)
+        {
+            System.IO.Stream s = Task.Run(() => FileSystem.OpenAppPackageFileAsync(Sound)).Result ;
+            AudioPlayer = AudioManager.CreatePlayer(s);
+            AudioPlayer.Play();
+        }
+
+    }
+}
