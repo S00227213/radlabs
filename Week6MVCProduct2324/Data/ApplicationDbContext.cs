8fe351b43d8c3b186b638ec026140876c116220c
Jack Monaghan <s00227213@atu.ie>
2023-11-07 14:20:19 +0000

    S00227213 Initial Project structure and Database Migrations complete

---
+++ Week6MVCProduct2324/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Week6MVCProduct2324.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet properties for your entities go here
+        // Example: public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
---
+++ Week6MVCProduct2324/Program.cs
@@ -0,0 +1,44 @@
+
+using Tracker.WebAPIClient;
+using Microsoft.EntityFrameworkCore;
+using Week6MVCProduct2324.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Register ApplicationDbContext with the DI container
+var connectionString = builder.Configuration.GetConnectionString("Week6MVCProduct23-S00227213") ?? throw new InvalidOperationException("Connection string 'Week6MVCProduct23-S00227213' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Change the Activity tracker message to "Seeding Application Table Data"
+ActivityAPIClient.Track(
+    StudentID: "S00227213",
+    StudentName: "Jack Monaghan",
+    activityName: "Rad301 23 Week 6 Lab 1",
+    Task: "Seeding Application Table Data");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
