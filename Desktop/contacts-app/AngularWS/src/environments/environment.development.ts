a29082436f9fe9fb2eaf70d16136905e82da1a8d
Jack Monaghan <s00227213@atu.ie>
2023-11-13 19:48:08 +0000

    Clean up repository

--- Desktop/contacts-app/AngularWS/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUri: 'http://localhost:3000/api/v1'  
-  };
-  
--- Desktop/contacts-app/AngularWS/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-interface Environment {
-  production: boolean;
-  apiUri: string;
-}
-
-export const environment: Environment = {
-  production: false,
-  apiUri: 'http://localhost:3000/api/v1'
-};
