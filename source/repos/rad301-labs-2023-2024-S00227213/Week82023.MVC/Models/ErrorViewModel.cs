6cd5227e0e9f0b8f12165c85e6e424b60dd361e6
S00227213 <s00227213@atu.ie>
2023-12-06 09:46:44 +0000

    Delete source/repos directory

--- source/repos/rad301-labs-2023-2024-S00227213/Week82023.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Week82023.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- source/repos/rad301-labs-2023-2024-S00227213/Week9.MVC.S00227213/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Week9.MVC.S00227213.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
