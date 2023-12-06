15c31537e9cebd5f8f752c3d844d0211cced3d36
Jack <s00227213@atu.ie>
2023-12-06 11:45:04 +0000

    Week 10 s00227213 Student Controller created and date pickers tested

--- Week10MVCS00227213/Program.cs
+++ Week10MVCS00227213/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using DataModel; 
+
 namespace Week10MVCS00227213
 {
     public class Program
@@ -9,13 +12,16 @@ namespace Week10MVCS00227213
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            // Add SchoolContext to the DI container
+            builder.Services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
